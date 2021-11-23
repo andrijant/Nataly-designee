2996ff43f90bb0ae83b15c95af449c34b19fafc9
andrijant <andrija_ds@yahoo.com>
2021-11-23 17:29:27 +0100

    change2-ajax

---
+++ productsDb.js
@@ -0,0 +1,17 @@
+// function loadData() {
+//   let xml = new XMLHttpRequest();
+//   let url = "https://raw.githubusercontent.com/andrijant/Nataly-designee/main/products.json";
+//   xml.open('GET', url);
+//   xml.onreadystatechange = function() {
+//     if (xml.readyState === 4 && xml.status === 200) {
+//       loadDataDb(JSON.parse(xml.responseText))
+//     }
+//   }
+//   xml.send();
+// };
+
+// function loadDataDb(dbProducts) {
+//     console.log(dbProducts);
+// };
+
+// loadData();
